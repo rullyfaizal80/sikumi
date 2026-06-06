385a16f86363e753b9a6cc1d019ef57513fdbba3
Rully Faizal <rullyfaizal@mimha.sch.id>
2026-06-06 19:57:22 +0700

    atp begin

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -179,3 +179,8 @@ $routes->post('perangkat/copy_draft', 'PerangkatController::copy_draft_elemen');
 $routes->post('admin/kaldik/category/store', 'KaldikController::storeCategory');
 $routes->post('admin/kaldik/category/update', 'KaldikController::updateCategory');
 $routes->get('admin/kaldik/category/delete/(:num)', 'KaldikController::deleteCategory/$1');
+
+// =========================================================================
+// ROUTE MODUL ALUR TUJUAN PEMBELAJARAN (ATP)
+// =========================================================================
+$routes->get('guru/atp', 'AtpController::index');
